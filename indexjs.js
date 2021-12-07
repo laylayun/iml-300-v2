81a82db098a8fcfc47caa3092bc74fd46f6fc628
laylayun <75710455+laylayun@users.noreply.github.com>
2021-12-06 23:03:10 -0800

    index page

---
+++ indexjs.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+  $(window).scroll(function () {
+    //        alert('You just scrolled!');
+    $("body").css(
+      "background",
+      "linear-gradient(0deg, rgba(235,226,208,0.7) 50%, rgba(89,147,104,0.5) 100%)"
+    );
+  });
+});
